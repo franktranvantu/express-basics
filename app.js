f3efd5ee7fe27a7cdda4e82d26a957e7633540c6
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-03-04 11:34:25 +0700

    Update

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  const error = new Error('Oops, something went wrong!');
-  error.status = 500;
-  next(error);
-});
-
-app.use((req, res, next) => {
-  console.log('World');
-  next();
-});
-
 app.get('/', (req, res) => {
   const name = req.cookies.username;
   if (name) {
@@ -53,6 +42,12 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
   res.clearCookie('username');
   res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
